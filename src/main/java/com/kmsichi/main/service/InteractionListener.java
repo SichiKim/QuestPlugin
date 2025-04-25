4469bd4c26bc90e959840c06f00a1b90245311ba
SichiKim <sky06sky@naver.com>
2025-04-25 21:58:04 +0900

    퀘스트 로직 개편

--- src/main/java/com/kmsichi/main/service/InteractionListener.java
+++ src/main/java/com/kmsichi/main/service/InteractionListener.java
@@ -1,35 +1,15 @@
 package com.kmsichi.main.service;
 
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.Player;
-import org.bukkit.event.player.PlayerInteractEntityEvent;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class NPCInteraction {
+public class InteractionListener {
 
     public static Map<String, Object> GUI_list = new HashMap<String, Object>();
     public static Map<String, Object> NPCDialog_list = new HashMap<String, Object>();
     public static HashMap<String, List<String>> dialog = new HashMap<String, List<String>>();
     public static HashMap<String, List<Integer>> acceptable = new HashMap<String, List<Integer>>();
-
-    // NPC와의 상호작용을 어떻게 작동하게 만들 것인가?
-    // 우클릭으로 상호작용을 해야 하기 때문에.. 일단 이벤트 구현이 필요하다.
-
-    public void onPlayerInteract(PlayerInteractEntityEvent e) {
-        Player p = e.getPlayer(); // 클릭한 플레이어
-        Entity en = e.getRightClicked(); // 클릭된 엔티티
-
-        //우리는 NPC가 우클릭 된 경우가 필요하기 때문에, 클릭 된 엔티티가 Player인 경우를 고려한다.
-        if (en instanceof Player) {
-            // 처음에는 엔티티별로 할당된 대사를 출력한다.
-            // 이를 위해 모든 NPC의 이름을 코드로 지정한다. town1npc1 => 001 식으로. 리스트 후술.
-
-            // 출력을 위해서 파일에서 NPC 코드의 대사를 불러온다. 이를 위해 yaml 데이터 처리를 위한 util 생성.
-        }
-    }
 
     /*
     public static void GUIManager() throws FileNotFoundException {
@@ -80,5 +60,6 @@ public class NPCInteraction {
                 }
                 break;
         }
-    }*/
+    }
+    */
 }
